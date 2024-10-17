4cb07bd831a63e2c25eaf9aaece4ae928fd770a1
amanrawt03 <amanrawat3021@gmail.com>
2024-10-17 14:04:36 +0530

    Finished  styling prime rentals

--- Angular/my-app/src/main.ts
+++ Angular/my-app/src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
